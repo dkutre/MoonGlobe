89a40accd9c0fac9d4a11ef4f44ff30eeafecc59
ovv <noking007@mail.ru>
2017-11-10 17:11:45 +0300

    Add FreeTypePlugin in ResourceManager

--- src/ResourceManager/ResourceManager.cpp
+++ src/ResourceManager/ResourceManager.cpp
@@ -3,12 +3,16 @@
 
 using namespace Moonglobe;
 
+std::shared_ptr<Magnum::Text::AbstractFont> ResourceManager::font_importer;
 std::shared_ptr<Magnum::Trade::AbstractImporter> ResourceManager::importer;
 ResourceManager* ResourceManager::manager = nullptr;
+
 ResourceManagerDestroyer ResourceManager::destroyer;
 
 ResourceManager::ResourceManager()
+    : font_is_loaded(false)
 {
+    // std::cout << "kek" << std::endl;
     fillHashTable(1, 2, 4);
     fillHashTable(2, 5, 10);
     fillHashTable(3, 10, 20);
@@ -33,14 +37,27 @@ void ResourceManager::fillHashTable(size_t zoom_level, size_t rows, size_t colum
     }
 }
 
+
 ResourceManager& ResourceManager::instance()
 {
-    if (importer == nullptr) {
-        Magnum::PluginManager::Manager<Magnum::Trade::AbstractImporter> plugin_manager;
-        importer = plugin_manager.loadAndInstantiate("JpegImporter");
+    // std::cout << "kek" << std::endl;
+    if (importer == nullptr &&
+        font_importer == nullptr)
+    {
+        Magnum::PluginManager::Manager<Magnum::Trade::AbstractImporter> importer_manager;
+        importer = importer_manager.loadAndInstantiate("JpegImporter");
         if (!importer) {
             std::exit(1);
         }
+        std::cout << "jpeg_importer_loaded\n" << std::endl;
+
+        Magnum::PluginManager::Manager<Magnum::Text::AbstractFont> font_plugin_manager;
+        font_importer = font_plugin_manager.loadAndInstantiate("FreeTypeFont");
+        if (!font_importer)
+            std::exit(1);
+
+        std::cout << "font_plugin_loaded\n" << std::endl;
+
     }
     if (manager == nullptr) {
         // std::cout << "\ncreate ResourceManager\n";
@@ -92,6 +109,8 @@ std::shared_ptr<Magnum::Texture2D> ResourceManager::loadTexture(TextureId id)
     if(!importer->openData(rs.getRaw(filename)))
         Magnum::Error() << "cannot load image\n";
 
+    std::cout << filename << std::endl;
+
     std::optional<Magnum::Trade::ImageData2D> image = importer->image2D(0);
     CORRADE_INTERNAL_ASSERT(image);
 
@@ -115,6 +134,31 @@ std::shared_ptr<Magnum::Texture2D> ResourceManager::loadTexture(TextureId id)
         .setSubImage(0, {}, *image);
 
     return new_texture;
+}
+
+void ResourceManager::fillGlyphCache(Magnum::Text::GlyphCache& cache)
+{
+    if (!font_is_loaded)
+        loadFont();
+
+    font_importer->fillGlyphCache(
+        cache,
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789:-+,.!°ěäЗдравстуймиγειασουτνκόμ "
+    );
+}
+
+std::shared_ptr<Magnum::Text::AbstractFont> ResourceManager::getFont()
+{
+    return font_importer;
+}
+
+void ResourceManager::loadFont()
+{
+    Magnum::Utility::Resource rs("fonts");
+    if(!font_importer->openSingleData(rs.getRaw("DejaVuSans.ttf"), 110.0f)) {
+        std::cout << "\nCannot open font file\n";
+        std::exit(1);
+    }
 }
 
 bool ResourceManager::freeTexture(TextureId id)
